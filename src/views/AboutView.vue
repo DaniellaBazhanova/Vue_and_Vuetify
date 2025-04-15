8dd76357a27b4ef6ad8a27b2921ad3a4ff73af8d
DaniellaBazhanova <bohdana.bazhanova@oa.edu.ua>
2025-04-15 15:43:28 +0300

    feat: done laboratory 4

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
   </div>
 </template>
@@ -10,6 +10,7 @@
     min-height: 100vh;
     display: flex;
     align-items: center;
+    justify-content: center;
   }
 }
 </style>
