37e1d6f9ee62561874b769456215c6e53a411bf8
DaniellaBazhanova <bohdana.bazhanova@oa.edu.ua>
2025-04-15 15:56:56 +0300

    feat: done laboratory 6

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import List from '../views/List.vue'
+import Group from '../views/Group.vue'
+import About from '../views/About.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,13 +12,17 @@ const router = createRouter({
       component: List,
     },
     {
+      path: '/group',
+      name: 'group',
+      component: Group,
+    },
+    {
       path: '/about',
       name: 'about',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/About.vue'),
-    },
+      component: About,    },
   ],
 })
 
